d52625f2df04436a724ead828547da9d5ac432ef
Ashish Singh <ashishsingh822003@gmail.com>
2023-11-30 17:45:18 +0530

    ok

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 
 require('dotenv').config()
+const app = require('./app.js')
 const connectDb = require('./db/dbconnect.js');
-connectDb(process.env.MONGODB_URI);
+
+port = process.env.PORT || 2000
+url = process.env.MONGODB_URI
+//asynchronous method on complete return a promise
+connectDb(url)
+.then(()=>{
+    app.listen(port , ()=>{
+        console.log(`server running at : ${port}`);
+    })
+})
+.catch((error)=>{
+    console.log('mongodb connection error ',error);
+})
 
