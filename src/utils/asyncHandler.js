f456dd7bdc4786b528d32be2605468890567c176
Ashish Singh <ashishsingh822003@gmail.com>
2023-12-01 18:22:45 +0530

    OK

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,8 +1,22 @@
+// by promises(resolve-reject)
 const asyncHandler = (requestHandler) => {
-    (req,res,next)=>{
-        Promise.resolve(requestHandler(req,res,next)).catch((error)=>{
-            next(error)
-        })
-    }
-}
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((error) => {
+            next(error);
+        });
+    };
+};
+// by try catch
+// const asyncHandler = (fn) => async(req,res,next) => {
+//     try{
+//         await fn(req,res,next)
+//     }catch(error){
+//         res.status(error.code || 500).json({
+//             success : false ,
+//             message : error.message
+//         })
+//     }
+// }
+
+
 module.exports = asyncHandler
