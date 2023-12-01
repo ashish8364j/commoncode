5edda77a433b7c354e5799b7929cd2700397c8a6
Ashish Singh <ashishsingh822003@gmail.com>
2023-12-01 12:16:01 +0530

    ok

--- src/app.js
+++ src/app.js
@@ -1,3 +1,25 @@
 const express = require('express');
+const cors = require('cors')
+//cookieParser used for acces cookie from user browser and set cookie(perform crud operation)
+const cookieParser = require('cookie-parser');
+
 const app = express();
+// * = kahin se bhi request aye dedo data
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+//when data come in the form of json
+//json form data ki limit kya hai = 16 kb
+app.use(express.json({limit:`${process.env.DATA_LIMIT}`}))
+
+//when data come from the url
+//extended use for nexted object
+app.use(express.urlencoded({extended:true,limit:`${process.env.DATA_LIMIT}`}))
+
+//when we want to store file folder
+app.use(express.static("public"))
+
+app.use(cookieParser())
+
 module.exports = app ;
